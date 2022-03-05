7060415db2ca0f187ae5f679cff1e1b110b6f8e3
Vanakoff <andrey.vanakoff@gmail.com>
2022-03-05 01:07:44 +0100

    new token test

--- SpacePhoto/PhotoInfoController.swift
+++ SpacePhoto/PhotoInfoController.swift
@@ -38,24 +38,6 @@ class PhotoInfoController: UIViewController {
         task.resume()
     }
     
-//    func updateUI(with photoInfo: PhotoInfo) {
-//        let task = URLSession.shared.dataTask(with: photoInfo.url, completionHandler: { data, response, error in
-//            if let data = data,
-//               let image = UIImage(data: data) {
-//                DispatchQueue.main.async {
-//                    self.title = photoInfo.title
-//                    self.imageView.image = image
-//                    self.descriptionLabel.text = photoInfo.description
-//                    if let copyright = photoInfo.copyright {
-//                        self.copyrightLabel.text = "Copyright \(copyright)"
-//                    } else {
-//                        self.copyrightLabel.isHidden = true
-//                    }
-//                }
-//            }
-//        })
-//        task.resume()
-//    }
     
     
 }
